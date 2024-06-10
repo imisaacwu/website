16af81631fbdb02dbd087307abf6b07d1ddd1413
Isaac <isaacw1925@gmail.com>
2024-06-10 15:27:03 -0700

    port from archive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from "./components/Navbar";
+import {Routes, Route} from "react-router-dom";
+import './css/styles.css';
+import Projects from "./components/Projects";
+import {Home} from "./components/Home";
+import {NoMatch} from "./components/NoMatch";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path={"/"} element={<Home />} />
+                <Route path={"website"} element={<Home />} />
+                <Route path={"projects"} element={<Projects />} />
+                <Route path={"*"} element={<NoMatch />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
