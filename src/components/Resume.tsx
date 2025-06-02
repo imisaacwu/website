be46d81d7ae85aca26a0d8804998d7e15f653e01
Isaac <isaacw1925@gmail.com>
2025-06-02 15:45:21 -0700

    Update resume

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -3,7 +3,7 @@ import './Resume.css'
 export const Resume = () => {
     return (
         <div className='container'>
-            <embed id='resume' src='./pdf/Isaac-Wu-Resume.pdf' />
+            <embed id='resume' src='./pdf/Isaac_Wu_resume.pdf' />
         </div>
     )
 }
