9afa72a2c9055449760dab197c68cdfefa16e388
Isaac Wu <isaacwu@uw.edu>
2026-07-08 09:00:36 -0700

    Update albums, add links

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,9 +1,13 @@
 import './Images.css'
 
-export const Images = ({images = []}: {images: string[]}) => {
+export const Images = ({images = [], links = []}: {images: string[], links?: string[]}) => {
     return (
         <div id='carousel'>
-            {images.map((url) => <img key={url} src={url} />)}
+            {images.map((img, i) => (links[i] ?
+                <a href={links[i]} target='_blank' rel='noreferrer'>
+                    <img key={img} src={img} />
+                </a>
+            : <img key={img} src={img} />))}
         </div>
     )
 }
